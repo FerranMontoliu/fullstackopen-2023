e22275232e3d28de3f6953b87a2210b3bf754c76
Ferran Montoliu <montoliuferran@gmail.com>
2023-08-12 18:29:21 +0300

    fix: Added key to part loop to prevent console errors.

--- exercises/part-1/course-information/src/components/Content.jsx
+++ exercises/part-1/course-information/src/components/Content.jsx
@@ -3,8 +3,8 @@ import Part from './Part'
 const Content = ({ course: { parts } }) => {
     return (
         <div>
-            {parts.map(part => (
-                <Part name={part.name} numExercises={part.exercises} />
+            {parts.map((part, i) => (
+                <Part key={i} name={part.name} numExercises={part.exercises} />
             ))}
         </div>
     )
